6ac5984f71d573ec51f2e369295c9c08c51857be
Chndn <chndnsharma5036@gmail.com>
2026-04-20 17:12:45 +0530

    made some changes

--- src/main/java/com/expensetracker/project/controller/RecurringRuleController.java
+++ src/main/java/com/expensetracker/project/controller/RecurringRuleController.java
@@ -1,29 +1,5 @@
-package com.chndn.expensetracker.controller;
+package com.expensetracker.project.controller;
 
-import com.chndn.expensetracker.entity.RecurringRule;
-import com.chndn.expensetracker.repository.RecurringRuleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/recurring")
 public class RecurringRuleController {
 
-    @Autowired
-    private RecurringRuleRepository ruleRepository;
-
-    // Create a new recurring rule (e.g., Monthly Netflix)
-    @PostMapping
-    public ResponseEntity<RecurringRule> createRule(@RequestBody RecurringRule rule) {
-        return ResponseEntity.ok(ruleRepository.save(rule));
-    }
-
-    // Get all active rules
-    @GetMapping("/active")
-    public ResponseEntity<List<RecurringRule>> getActiveRules() {
-        return ResponseEntity.ok(ruleRepository.findAll());
-    }
 }
